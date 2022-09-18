0764ce1cd25601f02dc357dce2dafc0d11f5e610
Kanwal <110118296+Queen123deep@users.noreply.github.com>
2022-09-18 11:08:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 // Songs Titles
-const songs = ["Pent Straight", "", ""];
+const songs = ["Pent_Straight", "", ""];
 // KeepTrack of song
 let songIndex = 0;
 // Initially load song details into DOM
